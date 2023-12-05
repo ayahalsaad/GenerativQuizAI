abbde8ae75096c5d8c44fa3b2dca5edc771953e5
ayahalsaad <ayahmustafa9@gmail.com>
2023-12-05 07:45:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import openai
+import streamlit as st
+
+from app.app import get_app
+
+OPENAI_TOKEN = "OPENAI_TOKEN"
+
+
+def initial_config():
+    """
+    Initial configuration of OpenAI API and streamlit
+    """
+    try:
+        openai.api_key = st.secrets["OPENAI_TOKEN"]["key"]
+
+
+    except KeyError:
+        st.error("OpenAI API key not found in secrets.toml.")
+        st.stop()
+
+    st.set_page_config(
+        page_title="Exam generator",
+        page_icon=":pencil2:",
+    )
+
+
+def main():
+    initial_config()
+    app = get_app()
+    app.render()
+
+
+if __name__ == '__main__':
+    main()
